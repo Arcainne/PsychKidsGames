e6c9f2335af8900f4311a09408c01c84659ed81a
cthurmond <connorthurmond@gmail.com>
2016-02-11 22:44:23 -0800

    Played with code
    Couldn't get anything important to work, but I was able to position some of the text differently and spent most of the rest of the time with no idea what was going on. I emailed Sean some questions, maybe he can help us.

--- InhibitionGame/js/states/Game.js
+++ InhibitionGame/js/states/Game.js
@@ -5,53 +5,48 @@
 
 // Create global empty Game state object
 var Game = function() {};
-
+var score = 0;
 // Define global Game state object
 Game.prototype = {
-	
+    
     init: function () {
-        // Create title text
-        this.scoreText = game.make.text(game.world.centerX, 40, "PLEASE WORK", {
+        // Create score text.
+        this.scoreText = game.make.text(10, 5, "Score: " + score, {
             font: 'bold 36pt Arial',
             fill: '#0000000',
-            align: 'center'
         });
         this.scoreText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
 
-        // Create instructions sprite object
-        
-
         // Create instructions text
         this.instructionsText = game.make.text(
-                game.world.centerX,
-                80,
+                250,
+                5,
                 "When the CAT appears, tap it for a point. Try to avoid tapping the GHOST.",
                 {
-                    font: 'bold 30pt Arial',
+                    font: 'bold 20pt Arial',
                     fill: 'black',
-                    align: 'center',
+                    align: 'left',
                     wordWrap: true,
-                    wordWrapWidth: game.world.width
+                    wordWrapWidth: game.world.width 
                 }
         );
-       this.instructionsText.anchor.set(0.5, 0);
 
         // Create cat sprite object
         this.catSprite = game.make.sprite(Math.random()*(game.world.width),
-        						Math.random()*(game.world.height), 'cat');
+                                Math.random()*(game.world.height), 'cat');
         this.catSprite.scale.set(0.5, 0.5);
 
         // Create ghost sprite object
         this.ghostSprite = game.make.sprite(Math.random()*(game.world.width),
-        						Math.random()*(game.world.height), 'ghost');
+                                Math.random()*(game.world.height), 'ghost');
         this.ghostSprite.scale.set(0.5, 0.5);
 
        
 
-        utilities.centerGameObjects([this.scoreText, this.catSprite, this.ghostSprite]);
+        utilities.centerGameObjects([this.catSprite, this.ghostSprite]);
     },
 
-      create: function () {
+    create: function () {
         // Make background color light blue
         game.stage.backgroundColor = '#99CCFF';
 
@@ -60,34 +55,43 @@ Game.prototype = {
         game.add.existing(this.instructionsText);
         game.add.existing(this.catSprite);
         game.add.existing(this.ghostSprite);
-      	this.catSprite.visible = false;
-      	this.ghostSprite.visible = false;
-      	this.catSprite.inputEnabled = true;
-      	this.ghostSprite.inputEnabled = true;
-      
+        this.catSprite.visible = false;
+        this.ghostSprite.visible = false;
+        this.catSprite.inputEnabled = true;
+        this.ghostSprite.inputEnabled = true;
+
     },
 
+    // play: function () {
+    //     console.log("Hello!")
+    // },
+    // function play() {
+    //     console.log("hello");
+    // },
+
     // Main update function that is called repeatedly
-    update: function () {
-        // Test code to move sprites randomly
-        var catOrGhost = Math.random()*10;
-        var newPosX, newPosY;
+    // update: function () {
+    //     // Test code to move sprites randomly
+    //     var catOrGhost = Math.random()*10;
+    //     var newPosX, newPosY;
 
-        if (catOrGhost > 9){
-        	newPosX = Math.random() * (game.world.width);
-        	newPosY = Math.random() * (game.world.height);
+    //     if (catOrGhost > 9){
+    //         newPosX = Math.random() * (game.world.width);
+    //         newPosY = Math.random() * (game.world.height);
 
-        	this.catSprite.position.x = newPosX;
-        	this.catSprite.position.y = newPosY;
-        	this.catSprite.visible = true;
-     
-        }
-        //  this.catSprite.x += Math.random() <= 0.5 ? -1 : 1;
-        // this.catSprite.y += Math.random() <= 0.5 ? -1 : 1;
-        // this.ghostSprite.x += Math.random() <= 0.5 ? -1 : 1;
-        // this.ghostSprite.y += Math.random() <= 0.5 ? -1 : 1;
-    }
-//    function play(){
-//     	console.log("hello");
-//     },
-};
+    //         this.catSprite.position.x = newPosX;
+    //         this.catSprite.position.y = newPosY;
+    //         this.catSprite.visible = true;
+        
+    //     }
+    //     //  this.catSprite.x += Math.random() <= 0.5 ? -1 : 1;
+    //     // this.catSprite.y += Math.random() <= 0.5 ? -1 : 1;
+    //     // this.ghostSprite.x += Math.random() <= 0.5 ? -1 : 1;
+    //     // this.ghostSprite.y += Math.random() <= 0.5 ? -1 : 1;
+    // },
+    
+    // function play(){
+    //     console.log("hello");
+    // },
+}
+
